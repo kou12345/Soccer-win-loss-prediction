d1c7a5899ba5d8c3e7d03dc5db7dd373ea2c269a
kou12345 <hi.0412.ko.2002@gmail.com>
2022-11-30 14:32:58 +0900

    チーム名を取得 TODOを追加

--- main.py
+++ main.py
@@ -6,24 +6,29 @@ driver_path = "./chromedriver.exe"
 driver = webdriver.Chrome()
 
 # urlを指定してブラウザで開く
-url = "https://soccer.yahoo.co.jp/ws/category/eng/schedule/202205201/16/?gk=52"
+url = "https://soccer.yahoo.co.jp/ws/category/eng/teams"
 driver.get(url)
 
-# print(driver.page_source)
+# チーム名の取得
+teams = driver.find_elements(By.CLASS_NAME, "sc-team__title")
 
-item = driver.find_element(By.CLASS_NAME, "sn-notice__text")
-print(item)
-print(type(item))
-print(item.text)
+teams_list = []  # チーム名のリスト
 
-items = driver.find_elements(By.CLASS_NAME, "sc-tableGame__team")
+for team in teams:
+    if team.text != "":
+        teams_list.append(team.text)
 
-for i in items:
-    print(i.text)
+print(f"teams_list: {teams_list}")
+
+# TODO 「チーム情報」をクリック
+# TODO 「sc-tableTeamRank__data」のデータを取得
+
 
 # points = driver.find_elements(By.CLASS_NAME, "sc-tableGame__scoreDetail")
 # for point in points:
 #     print(point.text)
 
-point = driver.find_element(By.CLASS_NAME, "sc-tableGame__scoreDetail")
-print(point.text)
+# スコアを取得 例 1 - 2
+# point = driver.find_element(By.CLASS_NAME, "sc-tableGame__scoreDetail")
+# print(point.text)
+
