1befdc3898fa9b750b07a9be05cd7053ca71dbc1
kou12345 <hi.0412.ko.2002@gmail.com>
2022-11-30 17:55:45 +0900

    全チームの順位データ取得

--- main.py
+++ main.py
@@ -21,22 +21,41 @@ for team in teams:
 
 print(f"teams_list: {teams_list}")
 
-team_item = driver.find_element(By.CLASS_NAME, "sc-team__item")
-# print(team_item.find_element(By.CLASS_NAME, "sc-team__menuItem").text)  # value: チーム情報
-team_info_url = team_item.find_element(By.TAG_NAME, "a").get_attribute("href")  # チーム情報のURL取得
-# 「チーム情報」へ遷移
-driver.get(team_info_url)
+print("test")
+all_team_items = driver.find_elements(By.CLASS_NAME, "sc-team__item")
 
-# 「順位」のデータを取得
-team_rank_info_col = driver.find_element(By.CLASS_NAME, "sc-tableTeamRank")
-print(team_rank_info_col.text)
-# 順位 勝点 試合数 勝数 引分数 敗数 得点 失点 得失点差
-# 14 16 15 4 4 7 18 32 -14
+all_team_url = []
 
+for i in all_team_items:
+    team_title = i.find_element(By.CLASS_NAME, "sc-team__title")
+    all_team_url.append(i.find_element(By.TAG_NAME, "a").get_attribute("href"))
+
+print(all_team_url)
+
+team_rank_info_list = []
+
+tmp_list = [[]]
+
+# 全チームの順位データを取得
+for team_url in all_team_url:
+    # 遷移
+    driver.get(team_url)
+    # 「順位」のデータを取得
+    # team_rank_info_list.append(driver.find_element(By.CLASS_NAME, "sc-tableTeamRank__head"))
+    # print(driver.find_element(By.CLASS_NAME, "sc-tableTeamRank").text)
+
+    # 成功
+    team_rank_info = driver.find_element(By.CLASS_NAME, "sc-tableTeamRank__row").text
+    print(driver.find_element(By.CLASS_NAME, "sc-tableTeamRank__row").text)
+
+    tmp_list.append(team_rank_info.split())
+
+print(tmp_list)
+print(len(tmp_list))
+
+# TODO tmp_listの先頭に空要素が入っている問題
 
 # TODO pandasでDataFrameに変換
 # TODO 取得した「順位」データをcsvファイルに保存
-# TODO 全チームの順位データを取得
-
 
 
